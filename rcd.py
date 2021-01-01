b271355887e9873d15b52dea4b40a4a48bde2a22
¢¥B€R Λ₤ÑØΜ <72166424+venomous-maker@users.noreply.github.com>
2021-01-01 15:41:22 +0300

    Create rcd.py

--- rcd.py
+++ rcd.py
@@ -10,24 +10,33 @@ CODED BY:
          https://darktelegramvenomous my channel
 """"")
 message = input('Type or paste the cipher here:''  ')
-translated = ''
+if len(message)==0:
+    print('          YOU MUST INSERT A VALID CIPHER!!')
+    message = input('Type a valid message  ')
+    translated = ''
+else:
+    translated = ''
+
 i = len(message) - 1
 while i >= 0:
     translated = translated + message[i]
     i = i - 1
-
-print('''\
+if len(message)>0:
+    print('''\
      ....................................................
      Your Encrypted Text Is:
-''')
-print("      "+translated)
-print('     ....................................................')
+    ''')
+    print("      "+translated)
+    print('     ....................................................')
+else:
+    print('      NO CIPHER INSERTED')
+
 print('''\
-__________________________________________________________
-__________________________________________________________
+___________________________________________________________
+___________________________________________________________
        T H A N K    Y O U    F O R   U S I N G    M E !
                   To be updated soon
                    Copy with credits
-__________________________________________________________
-__________________________________________________________
+___________________________________________________________
+___________________________________________________________
 ''')
